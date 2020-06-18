d9c3dc2e8ccf17108c2d3dcc332de81153c5da89
zoevincent <zoe.vincent@hetic.net>
2020-06-18 19:40:49 +0200

    map datas init and map update

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
+<?php
+include_once './includes/config.php';
+
+//****GET API DATAS WITH CURL METHOD***//
+// Instantiate curl
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, 'https://bridge.buddyweb.fr/api/gendergap/datascore');
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+$data = curl_exec($curl);
+curl_close($curl);
+?>
+
+<!-- Send datas to javascript -->
+<script>
+    const data = <?= json_encode($data); ?>
+</script>
+
+<!-- DOM -->
 <!DOCTYPE html>
 <html>
 
@@ -5,6 +23,7 @@
     <meta charset="utf-8">
     <title>European gender gap</title>
     <link rel="stylesheet" href="./src/styles/map.css">
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;700&display=swap" rel="stylesheet">
 </head>
 
 <body>
@@ -13,9 +32,17 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/topojson/1.6.9/topojson.min.js"></script>
     <script src="node_modules/datamaps/dist/datamaps.world.min.js"></script>
 
+    <!-- <div class="categories">
+        <button class="js-global">Global</button>
+        <button class="js-education">Education</button>
+        <button class="js-work">Work</button>
+        <button class="js-health">Health</button>
+        <button class="js-power">Power</button>
+        <button class="js-violence">Violence</button>
+    </div> -->
     <div id="container"></div>
 
-    <script src="./src/scripts/map.js"></script>
+    <script src="src/scripts/map.js"></script>
 </body>
 
 </html>
--- src/scripts/map.js
+++ src/scripts/map.js
@@ -1,15 +1,41 @@
+// Datas
+const datajson = JSON.parse(data)
+// const dataset_global = {}
+
+datajson.forEach((_country) => {
+    console.log(_country.country)
+    // console.log(_country.global_score)
+    // const data_global[_country] = { score: _country.global_score, fillColor: "#B3DDBF" };
+})
+
+// Datamaps expect data in format:
+// { "USA": { "fillColor": "#42a844", numberOfWhatever: 75},
+//   "FRA": { "fillColor": "#8dc386", numberOfWhatever: 43 } }
+
+// console.log(dataset_global)
+
+// map
 const map = new Datamap({
     element: document.getElementById('container'),
-    // projection: 'mercator',
+    // datas
+    // data: dataset_global,
+    // colors
+    fills: {
+        VERYHIGH: '#41A95E',
+        HIGH: '#67BA7E',
+        MEDIUM: '#8DCB9E',
+        LOW: '#B3DDBF',
+        VERYLOW: '#D9EEDF',
+        defaultFill: '#FFFFFF'
+    },
+    // Europe mercator view
     setProjection: function (element) {
-        const projection = d3.geo.equirectangular()
-            .center([15, 50])
-            // .rotate([4.4, 0])
-            .scale(1000)
+        const projection = d3.geo.mercator()
+            .center([15, 53])
+            .scale(950)
             .translate([element.offsetWidth / 2, element.offsetHeight / 2]);
         const path = d3.geo.path()
             .projection(projection)
-
         return { path: path, projection: projection };
     }
 });
