f208c317fbc4b722bd0d43427c82a2fc6206ff6d
Tina Elh <tinaelha@gmail.com>
2020-06-19 17:11:12 +0200

    Chart update :rocket:

--- testChart.php
+++ testChart.php
@@ -17,10 +17,6 @@ $datastudies = json_decode($datastudies);
 
 ?>
 
-
-
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -30,62 +26,75 @@ $datastudies = json_decode($datastudies);
     <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
 </head>
 <body>
-        <canvas id="myChart"></canvas>
+<canvas id="myChart" width="800" height="450"></canvas>
 
+<!-- <?php foreach ($datastudies as $key => $dataItem):?>
+  <p><?= $dataItem->year ?></p>
+<?php endforeach ?> -->
 
-<?php
+<script>
+  const ctx = document.getElementById('myChart').getContext('2d')
 
-foreach ($datastudies as $dataItem){
+  // beginning of Chart generation
+  const myChart = new Chart(ctx, {
+  type: 'line',
 
-?>
+    data: {
+      // Years
+      labels: 
+      [
+        <?php foreach ($datastudies as $dataItem):?>
+          <?= $dataItem->year.',' ?>
+        <?php endforeach ?>
+      ],
 
-        <script>
-            var ctx = document.getElementById('myChart').getContext('2d');
-    
-
-            // beginning of Chart generation
-
-            var myChart = new Chart(ctx, {
-            type: 'line',
-            
-            data: {
-                labels: ['<?= $dataItem->year ?>'],
-            
-
-                datasets: [{
-       
-                label: '<?= $dataItem->sex ?>',
-                data: [<?= $dataItem->value ?>],
-                backgroundColor: "rgba(153,255,51,0.4)"
-                }, {
-    
-    
-                label: 'Hommes',
-                data: [20, 39, 45, 55, 72],
-                backgroundColor: "rgba(255,153,0,0.4)"
-                }]
-            }, 
-
-
-            options: {
-          legend: { display: true },
-          title: {
-            display: true,
-            text: 'Predicted world population (millions) in 2050'
+      datasets: [{
+      // Woman
+      label: 'Woman',
+      data: 
+      [
+        <?php foreach ($datastudies as $dataItem):?>
+          <?php 
+          if($dataItem->sex === 'W')
+          {
+          ?>
+            <?= $dataItem->value.',' ?>
+          <?php
           }
-        }
-    
-    
-            });
-    
-    
-    </script>
-   
+          ?>
+        <?php endforeach ?>
+      ],
+      backgroundColor: "rgba(153,255,51,0.4)"
+      }, {
 
-   <?php
-}
-?>
+      // Men
+      label: 'Men',
+      data: 
+      [
+        <?php foreach ($datastudies as $dataItem):?>
+          <?php 
+          if($dataItem->sex === 'M')
+          {
+          ?>
+            <?= $dataItem->value.',' ?>
+          <?php
+          }
+          ?>
+        <?php endforeach ?>
+      ],
+      backgroundColor: "rgba(255,153,0,0.4)"
+      }]
+    },
 
+    options: {
+      legend: { display: true },
+      title: {
+        display: true,
+        text: 'Predicted world population (millions) in 2050'
+      }
+    }
+  })
+</script>
 
 </body>
 </html>
