71431f857232856e66914ff0cb658a5044451f40
CatincaG <catincag@users.noreply.github.com>
2020-06-24 00:45:35 +0200

    :lipstick: homepage update

--- index.php
+++ index.php
@@ -3,9 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="./src/styles/main.css">
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;700&display=swap" rel="stylesheet">
+    <title>Welcome !</title>
 </head>
 <body>
-    <p>This site has been created for educational purposes as part of the Grande Ecole curriculum of the HETIC school. The contents and data presented have not been the subject of a request for right of use. This site will in no way be exploited for commercial purposes.</p>
+    <img src="./assets/svg/illustrations/eu-stars.svg" alt="European Union stars" class="eu-stars">
+    <img src="./assets/svg/illustrations/first-page-all-content.svg" alt="Woman and man" class="woman-man">
+    <main class="first-page-container">
+        <h1>Equally</h1>
+        <div class="project-description">
+            <p>In Europe, gender equality is guaranteed by laws and charters. In recent years, we have been talking a lot about it and there are more and more demonstrations. Action is being taken and we are led to believe that everything is going well.</p>
+            <p>But even in 2020 and in Europe, these inequalities are still shocking. On a familial, professional or political level, women are still largely disadvantaged.</p>
+        </div>
+        <a href="map.php"><div class="discover-button">Check the data</div></a>
+    </main>
+    <p class="last-sentence">This site has been created for educational purposes as part of the Grande Ecole curriculum of the HETIC school. The contents and data presented have not been the subject of a request for right of use. This site will in no way be exploited for commercial purposes.</p>
 </body>
 </html>
